839f2e4c4c145fb035960c230b3baca96a509e79
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-08-30 10:32:12 +0100

    Display final score on gameresult.html page

--- assets/js/gameresult.js
+++ assets/js/gameresult.js
@@ -2,3 +2,6 @@ console.log("Hello from gameresult.html")
 
 var gameresult = sessionStorage.getItem("gameresult");
 console.log("Your score is " + gameresult);
+
+result = document.getElementById("result");
+result.innerText = `Your score is " ${gameresult}`;
