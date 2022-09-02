73ae4f0f01a16bc95d3152e77f87ade850c97b63
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-09-02 21:33:37 +0100

    Add function to play Audio file at quize end

--- assets/js/gameresult.js
+++ assets/js/gameresult.js
@@ -3,5 +3,8 @@ console.log("Hello from gameresult.html")
 var gameresult = sessionStorage.getItem("gameresult");
 console.log("Your score is " + gameresult);
 
+const surprise = new Audio("assets/audio/surprise.mp3");
+surprise.play();
+
 result = document.getElementById("result");
 result.innerHTML = `You scored<br> <span class="quiz-result">${gameresult}</span><br> points`;
