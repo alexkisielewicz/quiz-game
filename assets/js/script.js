711547a08622ce6f29b08dfdae194708749da290
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-09-04 21:13:04 +0100

    Remove arrays with audio files from the file
    - to be copied into separate JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// wait for DOM to load first
+// wait for the DOM to load first
 window.addEventListener('DOMContentLoaded', () => {
   const startButton = document.getElementById("startButton");
   const rulesButton = document.getElementById("rulesButton");
@@ -42,39 +42,6 @@ pauseButton.addEventListener("click", function () {
   }
 });
 
-// Sounds to play when user gives correct answer
-const audioFilesCorrect = [
-    "assets/audio/good1.mp3",
-    "assets/audio/good2.mp3",
-    "assets/audio/good3.mp3",
-    "assets/audio/good4.mp3",
-    "assets/audio/good5.mp3",
-    "assets/audio/good6.mp3",
-    "assets/audio/good7.mp3",
-    "assets/audio/good8.mp3",
-    "assets/audio/good9.mp3",
-    "assets/audio/good10.mp3"
-]
-
-// Sounds to play when user gives incorrect answer
-const audioFilesIncorrect = [
-    "assets/audio/wrong1.mp3",
-    "assets/audio/wrong2.mp3",
-    "assets/audio/wrong3.mp3",
-    "assets/audio/wrong4.mp3",
-    "assets/audio/wrong5.mp3",
-    "assets/audio/wrong6.mp3",
-    "assets/audio/wrong7.mp3",
-    "assets/audio/wrong8.mp3",
-    "assets/audio/wrong9.mp3",
-    "assets/audio/wrong10.mp3",
-    "assets/audio/wrong11.mp3",
-    "assets/audio/wrong12.mp3",
-    "assets/audio/wrong13.mp3",
-    "assets/audio/wrong14.mp3",
-    "assets/audio/wrong15.mp3"
-]
-
 // Query DOM for choices and create array with answer buttons
 const choices = Array.from(document.getElementsByClassName("choice"));
 
@@ -94,6 +61,7 @@ function startGame() {
 }
 
 function correctAnswer() {
+  console.log('%c Correct answer! ', 'background: #0F0; color: #000')
   correctSound.play();
   userScore++;
   correctAnswerButton = (choices[rightAnswer - 1]);
@@ -111,6 +79,7 @@ function correctAnswer() {
 }
 
 function incorrectAnswer() {
+  console.log('%c Wrong answer! ', 'background: #F00; color: #FFF')
   incorrectSound.play();
   correctAnswerButton = (choices[rightAnswer - 1]);
   correctAnswerButton.classList.add("correct");
@@ -165,6 +134,7 @@ function showQuestion() {
   
   choices.forEach((choice) => {
     const optionIndex = choice.dataset.answer;
+    // insert answer options from the array into the buttons in html 
     choice.innerText = currentQuestion["option" + optionIndex];
   });
 
@@ -214,7 +184,7 @@ choices.forEach((choice) => {
       return window.location.href = "score.html";
       //return alert("Your score is" + " " + gameResult);
     }
-    console.log("Questions left " + availableQuestions.length );
+    console.log("Questions left in the array: " + availableQuestions.length );
     
   });
 });
