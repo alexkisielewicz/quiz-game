29d98dad14361f0db54211fa34248a91dbcd2ab5
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-09-04 21:13:38 +0100

    Create new JS file containing arrays with sounds

---
+++ assets/js/sounds.js
@@ -0,0 +1,34 @@
+console.log("Sounds loaded!");
+
+// Sounds to play when user gives correct answer
+const audioFilesCorrect = [
+    "assets/audio/good1.mp3",
+    "assets/audio/good2.mp3",
+    "assets/audio/good3.mp3",
+    "assets/audio/good4.mp3",
+    "assets/audio/good5.mp3",
+    "assets/audio/good6.mp3",
+    "assets/audio/good7.mp3",
+    "assets/audio/good8.mp3",
+    "assets/audio/good9.mp3",
+    "assets/audio/good10.mp3"
+]
+
+// Sounds to play when user gives incorrect answer
+const audioFilesIncorrect = [
+    "assets/audio/wrong1.mp3",
+    "assets/audio/wrong2.mp3",
+    "assets/audio/wrong3.mp3",
+    "assets/audio/wrong4.mp3",
+    "assets/audio/wrong5.mp3",
+    "assets/audio/wrong6.mp3",
+    "assets/audio/wrong7.mp3",
+    "assets/audio/wrong8.mp3",
+    "assets/audio/wrong9.mp3",
+    "assets/audio/wrong10.mp3",
+    "assets/audio/wrong11.mp3",
+    "assets/audio/wrong12.mp3",
+    "assets/audio/wrong13.mp3",
+    "assets/audio/wrong14.mp3",
+    "assets/audio/wrong15.mp3"
+]
