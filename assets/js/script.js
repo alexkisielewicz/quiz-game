8a47bfe8bba5b622b00e1d9cc76b9767767edb48
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-09-05 19:13:37 +0100

    Add docstrings description to all functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,7 +53,9 @@ function showRules() {
    rulesButton.classList.add("hide");
 }
 
-
+/**
+ * This function iniciate the game by hiding the buttons from the main screen and showing game interface, qustion and answers. It also plays audio and creates new array with all questions. 
+ */
 function startGame() {
    console.log("Started!");
    // Hide quiz interface elements in HTML document
@@ -67,7 +69,9 @@ function startGame() {
    showQuestion();
 }
 
-
+/**
+ * The function is called when user gives correct answer. It increments the score and gives user feedback by playing the sound and changing body background and button colour in the DOM. After short delay it brings back the default style of a document.   
+ */
 function correctAnswer() {
    console.log('%c Correct answer! ', 'background: #0F0; color: #000');
    // Play audio to give user feedback for correct answer
@@ -96,7 +100,9 @@ function correctAnswer() {
    }, 2000);
 }
 
-
+/**
+ * The function is called when user gives incorrect answer. It gives user feedback by playing the sound and changing body background and button colour in the DOM. After short delay it brings back the default style of a document.
+ */
 function incorrectAnswer() {
    console.log('%c Wrong answer! ', 'background: #F00; color: #FFF');
    // Play audio to give user feedback for incorrect answer
@@ -121,7 +127,9 @@ function incorrectAnswer() {
    }, 2000);
 }
 
-
+/**
+ * Function is called to display the question in HTML document. It gets the random question from the array with all questions and inserts it in the quiz interface together with four answers. The question is removed from the array after user gives the answer. It also changes the width of progress bar in the quiz interface.  
+ */
 function showQuestion() {
    console.log("Showing random question");
 
@@ -138,10 +146,12 @@ function showQuestion() {
    // Get random audio file from all "Correct answer" sounds and assign to variable
    randomAudioFileCorrect = audioFilesCorrect[Math.floor(Math.random() * audioFilesCorrect.length)];
    correctSound = new Audio(randomAudioFileCorrect);
+   console.log("random CORRECT audio file to be played: " + randomAudioFileCorrect);
 
    // Get random audio file from all "Incorrect answer" sounds and assign to variable
    randomAudioFileIncorrect = audioFilesIncorrect[Math.floor(Math.random() * audioFilesIncorrect.length)];
    incorrectSound = new Audio(randomAudioFileIncorrect);
+   console.log("random INCORRECT audio file to be played: " + randomAudioFileIncorrect);
 
    // Get random question from the array with all questions (in questions.js)
    questionIndex = Math.floor(Math.random() * availableQuestions.length);
@@ -150,6 +160,7 @@ function showQuestion() {
    question.innerText = currentQuestion.question;
 
    // Show correct answer in the console for developer's easy reference
+   // TODO: remove this before release
    rightAnswer = currentQuestion.answer;
    console.log('%c Correct answer is option ', 'background: #222; color: #bada55', rightAnswer);
 
@@ -166,7 +177,9 @@ function showQuestion() {
    letUserClick = true;
 }
 
-
+/** 
+ * This function adds event listeners for user's clicks on the answer buttons and also checks if user picked correct answer. Function showing next question is called with delay so user is able to see visual feedback after giving answer. The score value is saved in local storage for further use on game result page. 
+ */
 function computeAnswer(event) {
    // Add event listener for choice buttons (data-answer="x")
    userChoice = event.target;
@@ -188,6 +201,11 @@ function computeAnswer(event) {
    sessionStorage.setItem("gameresult", gameResult);
 }
 
+/** 
+ * The function queries the DOM for answer buttons and disable/enable them after user gives answer. That prevents user from clicking on other buttons before answer is checked and feedback is displayed. 
+ * Further description available in Readme, bugs section.  
+ * Mentor - Reuben Ferrante edited this function and helped to fix the issue with multiple-selection of a buttons.  
+ */
 function enableDisableButtons(event, disable = true) {
    allButtons = document.getElementsByClassName("button choice");
    for (i=0; i < allButtons.length; i++) {
@@ -202,42 +220,29 @@ function enableDisableButtons(event, disable = true) {
          allButtons[i].removeAttribute("disable");
       }
    }
-}
+};
 
-
+/** 
+ * For each method is used to add event listeners for user clicks on the choices. If buttons are enabled it disables them after user click. This prevents user from clicking on other choices and displaying feedback multiple times. There are 20 questions in the array and user answers 10 random questions. When user answers the last tenth question the quiz ends and score button is displayed.    
+ * Mentor - Reuben Ferrante edited the code with first "if" condition.  
+ */
 choices.forEach((choice) => {
    choice.addEventListener("click", (event) => {
       // In case the user clicks again, check for disable attribute
       if (letUserClick && event.target.getAttribute("disable") !== '') {
          enableDisableButtons(event);
+         console.log("Score is: " + userScore);
          // Taking a sample of 10/20 questions
          // TODO This needs to be moved to showQuestions
-         if (availableQuestions.length <= 9) {
-            // Show the "Show Score Button"
-            return window.location.href = "score.html";
+         if (availableQuestions.length == 10) {
+            alert("Your score is: " + userScore);
+            //return window.location.href = "score.html";
          } 
          computeAnswer(event);
          console.log("Questions left in the array: " + availableQuestions.length);
       }
+
       letUserClick = false
 
    });
 });
-
-
-// Issues:
-// - choices stays selected on touch devices (can see it in DevTools too)
-// - max possible score is 9 instead of 10 
-// - no delay to see if the last 10th question was answered correctly
-// - clicking should be disabled after choice is clicked
-
-
-// FOR README: 
-// random audio array idea --> https://stackoverflow.com/questions/27053633/how-to-make-an-array-of-audio-files-randomly-in-javascript
-// mp3 source --> https://archive.org/details/tvtunes_6995
-// bg png source --> https://www.stickpng.com/
-// local storage --> https://lage.us/Javascript-Pass-Variables-to-Another-Page.html
-// audio files --> https://tuna.voicemod.net/search?search=bond
-// audio files --> https://www.soundboard.com/sb/jamesbond
-// audio files --> https://www.voicy.network/
-// favicon --> https://icons8.com/
