7f53c509cf797822dfc0592bbba787fed7b3992e
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-09-06 14:42:39 +0100

    Add conditional statement to display message depending on score

--- assets/js/gameresult.js
+++ assets/js/gameresult.js
@@ -1,10 +1,24 @@
 console.log("Hello from gameresult.html")
 
-var gameresult = sessionStorage.getItem("gameresult");
-console.log("Your score is " + gameresult);
-
+// Assign variable with audio and play it
 const surprise = new Audio("assets/audio/surprise.mp3");
 surprise.play();
 
+// Read user score saved in local storage and asssign it to variable
+const gameresult = sessionStorage.getItem("gameresult");
+console.log("Your score is " + gameresult);
+
+// Query DOM 
+const playAgain = document.getElementById("playAgain");
+playAgain.innerText = "Play Again";
+
+// Query DOM and display game result in HTML document
 result = document.getElementById("result");
-result.innerHTML = `You scored<br> <span class="quiz-result">${gameresult}</span><br> points`;
+
+if (gameresult === "10") {
+  result.innerHTML = `All answers correct <br><br> is there something you don't know about James Bond?`;
+  document.body.style.background = "#FFD700 ";
+} else {
+  result.innerHTML = `You scored<br> <span class="quiz-result">${gameresult}</span><br> points`
+}; 
+
