a130b7a314e2b1c898b598517792ae7011681fc9
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2022-09-07 16:28:33 +0100

    Change text of the message to be displayed on result page

--- assets/js/gameresult.js
+++ assets/js/gameresult.js
@@ -12,7 +12,7 @@ playAgain.addEventListener("click", replay);
 // Query DOM, change background colour and display text in html document depending on score value
 result = document.getElementById("result");
 if (gameresult === "10") {
-  result.innerHTML = `<span class="max-score">All answers correct</span> <br><br> is there something you don't know about James Bond?`;
+  result.innerHTML = `<span class="max-score">All answers are correct</span> <br><br> is there something about James Bond that you don't know?`;
   document.body.style.background = "#C6A226";
 } else {
   result.innerHTML = `You scored<br> <span class="quiz-result">${gameresult}</span><br> points`
